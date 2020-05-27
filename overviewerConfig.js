6b6ebeb330727009eb59c023b714e70afef8361a
C-Hickey <c.hickey@warwick.ac.uk>
2020-05-27 12:12:05 +0100

    autoCommit 20200527.121204

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1590576646",
+        "cacheTag": "1590577915",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1590576387,
+            "last_rendertime": 1590577488,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
